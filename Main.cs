89b4982406bb214534304995e28d2f14c61cbc14
Madison Tibbett <Esherymack@users.noreply.github.com>
2019-02-21 14:33:56 -0500

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,252 @@
+﻿/* CSMud - Rev 1
+ * Author: Madison Tibbett
+ * Last Modified: 02/20/2019
+ */
+
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+
+namespace CSMud
+{
+    /* The Server class is a simple TCP server to accept connections
+     * Used a lock to hold everything process-y
+     * Server also holds the main function.
+     * this is very largely based off of this forum post:
+     * https://bytes.com/topic/c-sharp/answers/275416-c-mud-telnet-server
+     * (okay, like 85%)
+     * and also from Microsoft's notes:
+     * https://docs.microsoft.com/en-us/dotnet/csharp/
+     * 
+     * My major modifications are as follows:
+     * - Moved the locks down into the methods that actually affect connections, cleaner code
+     * - Added 'user' name for differentiating between users
+     * - Changed ProcessLine to handle all incoming messages, differentiate between messages and commands
+     * - Designated SendMessage for sending chat messages.
+     * - Added connection and disconnection messages on the server for other users
+     */
+    public class Server
+    {
+        // define static port number and backlog size
+        const int port = 8088;
+        const int backLog = 20;
+
+        // main 
+        static void Main(string[] args)
+        {
+            /* 
+             * Instantiate Socket object 'server'
+             * 'AddressFamily.InterNetwork' refers to the AddressFamily enum - specifically addresses for IPv4
+             * 'SocketType.Stream' refers to the SocketType enum - Stream supports two-way conn-based byte streams without duplication of data
+             * 'ProtocolType.Tcp' refers to the ProtocolType enum - Tcp is a TCP server
+             */            
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            /*
+             * Socket.Bind method takes a local endpoint
+             * IPEndPoint class represents a network endpoint as an IP address and port number
+             */
+            server.Bind(new IPEndPoint(IPAddress.Any, port));
+            /*
+             * Socket.Listen method takes an integer and places the socket into a listening state. 
+             */
+            server.Listen(backLog);
+            while(true)
+            {
+                /*
+                 * Accept incoming connections and bring them down to Connection 
+                 */
+                Socket conn = server.Accept();
+                new Connection(conn);
+            }
+        }
+    }
+
+    /* Connection is basically the "client" - you don't have to have it separate
+     * Just telnet the connection
+     * 'telnet 192.168.1.111 8088' for example
+     */
+    public class Connection
+    {
+        /* We use a lock statement for the connections
+         * Lock statements are used in threading, and limit the number of threads that can perform an activity at any time.
+         * The benefit of this is that we can ensure that one thread does not enter a critical section while another thread is in that critical section.
+         *
+         * We need a new Socket, as well as a StreamReader and StreamWriter 
+         * StreamReader class reads characters from a byte stream in a particular encoding
+         * StreamWriter writes characters to a stream in a particular encoding
+         */
+        Socket socket;
+        public StreamReader Reader;
+        public StreamWriter Writer;
+        readonly string user;
+        /*
+         * Last, we need an ArrayList to hold our connections
+         */
+        static ArrayList connections = new ArrayList();
+
+        /*
+         * Constructor
+         */
+        public Connection(Socket socket)
+        {
+            this.socket = socket;
+            Reader = new StreamReader(new NetworkStream(socket, false));
+            Writer = new StreamWriter(new NetworkStream(socket, true));
+            Writer.AutoFlush = true;
+            user = GetLogin();
+            Beat();
+            /*
+             * Start a new Thread running ClientLoop()
+             */
+            new System.Threading.Thread(ClientLoop).Start();
+        }
+
+        /*
+         * ClientLoop basically handles the sending and receiving of messages as well as handling incoming and outgoing connections.
+         */
+        void ClientLoop()
+        {
+            try
+            {
+                OnConnect();
+                while(true)
+                {
+                    foreach(Connection conn in connections)
+                    {
+                        conn.Writer.Flush();
+                    }
+                    string line = Reader.ReadLine();
+                    if(line == null || line == "quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ProcessLine(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+            finally
+            {
+                Writer.WriteLine($"{user} has disconnected.");
+                socket.Close();
+                OnDisconnect();
+            }
+        }
+
+        /*
+        * GetLogin gets a screen name for individual users. This name is displayed for messages sent, actions, and connection/disconnection
+        */
+        string GetLogin()
+        {
+            Writer.Write("Please enter a name: ");
+            string userName = Reader.ReadLine();
+            return userName;
+        }
+
+        /*
+         * OnConnect handles the welcome messages and tells the server client that someone has connected.
+         */
+        void OnConnect()
+        {
+            Writer.WriteLine("Welcome!");
+            Writer.WriteLine("Send 'quit' to exit.");
+            Console.WriteLine($"{user} has connected.");
+            lock(connections)
+            {
+                connections.Add(this);
+            }
+        }
+
+        /*
+         * OnDisconnect handles removing the terminated connections and tells the sever client that someone has disconnected.
+         */
+        void OnDisconnect()
+        {
+            lock(connections)
+            {
+                connections.Remove(this);
+            }
+            Console.WriteLine($"{user} has disconnected.");
+        }
+
+        /*
+         * ProcessLine handles organizing messages on the MUD server
+         */
+        void ProcessLine(string line)
+        {
+            line = line.Trim();
+            SendMessage(line);
+        }
+
+        /*
+         * SendMessage handles sending speech messages on the MUD server
+         */
+        void SendMessage(string line)
+        {
+            lock(connections)
+            {
+                foreach(Connection conn in connections)
+                {
+                    conn.Writer.WriteLine($"{user} says, '{line}'");
+                }
+            }
+        }
+
+        void Beat()
+        {
+            Timer tmr = new Timer(45000);
+            tmr.Elapsed += OnTimedEvent;
+            tmr.AutoReset = true;
+            tmr.Enabled = true;
+        }
+
+        void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Writer.WriteLine("The world is dark and silent.");
+        }
+    }
+
+    // The Commands class holds all user commands other than movement
+    public class Commands
+    {
+    }
+
+    // The Movement class is movement-specific commands
+    public class Movement
+    {
+    }
+
+    // The Map class is all map-generating specifics
+    public class Map
+    {
+    }
+
+    // The Objects class holds all interactable object interactions
+    public class Objects
+    {
+    }
+
+    // The Entity class holds NPC and enemy specifics
+    public class Entity
+    {
+    }
+
+    // The Timer class simply starts and stops a timer for the run.
+    public class MapTimer
+    {
+    }
+
+    // The Combat class holds commands specific to combat. 
+    public class Combat
+    {
+    }
+}
