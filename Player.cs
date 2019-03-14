a0680f80898ffc77b7ff8c18be1d3e18f5acd07b
Esherymack <esherymack@gmail.com>
2019-03-14 11:42:08 -0400

    Added player and stats classes

---
+++ Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSMud
+{
+    /* The Player object houses the current User's appearance, statistics, and currently equipped and held Things
+     */   
+    public class Player
+    {
+        public Player()
+        {
+        }
+    }
+}
---
+++ Stats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CSMud
+{
+    /* The stats class houses statistics for players including health pool and damage output. */
+    public class Stats
+    {
+        public Stats()
+        {
+        }
+    }
+}
