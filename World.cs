6a6d61a7c0f508efc536dc7b997fae4eaaed9117
Esherymack <esherymack@gmail.com>
2019-03-28 12:17:50 -0400

    Players can now only hold 2 items at once, as they are humans and have two hands.

--- World.cs
+++ World.cs
@@ -496,43 +496,43 @@ namespace CSMud
             }
         }
 
-            void HandleEquip(User sender, string e)
+        void HandleEquip(User sender, string e)
+        {
+            var target = sender.Inventory.Things.FirstOrDefault(t => string.Equals(t.Name, e.Trim(), StringComparison.OrdinalIgnoreCase));
+            if (target == null)
             {
-                var target = sender.Inventory.Things.FirstOrDefault(t => string.Equals(t.Name, e.Trim(), StringComparison.OrdinalIgnoreCase));
-                if (target == null)
+                sender.Connection.SendMessage("You must have the object in your inventory.");
+            }
+            else if (target.IsWearable)
+            {
+                if(sender.Player.Equipped.Count == 6)
                 {
-                    sender.Connection.SendMessage("You must have the object in your inventory.");
-                }
-                else if (target.IsWearable)
-                {
-                    if(sender.Inventory.Things.Count == 6)
-                    {
-                        sender.Connection.SendMessage("You have no other room to wear that.");
-                    }
-                    else
-                    {
-                        bool isWearing = false;
-                        foreach (Thing thing in sender.Player.Equipped)
-                        {
-                            if (target.Slot == thing.Slot)
-                            {
-                                sender.Connection.SendMessage($"You are already wearing a {target.Slot} item.");
-                                isWearing = true;
-                                break;
-                            }
-                        }
-                        if(!isWearing)
-                        {
-                            sender.Inventory.RemoveFromInventory(target);
-                            sender.Player.Equip(target);
-                        }
-                    }
+                    sender.Connection.SendMessage("You have no other room to wear that.");
                 }
                 else
                 {
-                    sender.Connection.SendMessage("You cannot wear that.");
+                    bool isWearing = false;
+                    foreach (Thing thing in sender.Player.Equipped)
+                    {
+                        if (target.Slot == thing.Slot)
+                        {
+                            sender.Connection.SendMessage($"You are already wearing a {target.Slot} item.");
+                            isWearing = true;
+                            break;
+                        }
+                    }
+                    if(!isWearing)
+                    {
+                        sender.Inventory.RemoveFromInventory(target);
+                        sender.Player.Equip(target);
+                    }
                 }
             }
+            else
+            {
+                sender.Connection.SendMessage("You cannot wear that.");
+            }
+        }
 
             void HandleRemove(User sender, string e)
             {
@@ -561,8 +561,15 @@ namespace CSMud
                 }
                 else if (target.Commands.Contains("hold"))
                 {
-                    sender.Inventory.RemoveFromInventory(target);
-                    sender.Player.Hold(target);
+                    if(sender.Player.Held.Count == 2)
+                    {
+                        sender.Connection.SendMessage("You have no free hands.");
+                    }
+                    else
+                    {
+                        sender.Inventory.RemoveFromInventory(target);
+                        sender.Player.Hold(target);
+                    }
                 }
                 else
                 {
