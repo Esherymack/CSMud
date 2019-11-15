fc4f4c85eb9b25738e796d9227b5a1b636218274
Madison Tibbett <Esherymack@users.noreply.github.com>
2019-11-15 13:09:04 -0500

    made some changes; might drop this repo to refactor into another

---
+++ CSMudLib/src/utils/mb2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using CSMud.Entity;
+using System.IO;
+
+namespace CSMudLib.Utils
+{
+    public class mb2
+    {
+        public List<Item> Items { get; set; }
+        public List<NPC> NPCs { get; set; }
+        public List<NPC> DeadNPCs { get; set; }
+        public List<Door> Doors { get; set; }
+        public List<Room> Rooms { get; set; }
+        public Dictionary<int, Item> AllItems { get; set; }
+
+        public void LoadJson(string filename)
+        {
+
+            using (StreamReader r = new StreamReader(filename))
+            {
+                string json = r.ReadToEnd();
+                
+            }
+        }
+
+    }
+}
